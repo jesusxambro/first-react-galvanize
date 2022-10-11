ec9de490073a1a6d5cb067bd65f0356bb8f58100
Jesus <jesus.ambrocio@outlook.com>
2022-10-11 16:15:07 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState, useContext } from "react";
+
+
+
+
+class GroceryListItem extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state ={
+            done:true,
+            itemName: this.props.name
+            // key:this.props.key
+        };
+    }
+
+    onMouseHover(){
+        this.setState({
+            done: !this.state.done
+        })
+    }
+
+
+    render() {
+        let style = {
+            fontWeight: this.state.done ? "normal" : "bold"
+        }
+        return (
+            <li  key={this.itemName} style={style} onMouseOver={this.onMouseHover.bind(this)} onMouseLeave={this.onMouseHover.bind(this)}>
+                {this.state.itemName}
+            </li>
+        );
+    }
+}
+
+class GroceryList extends React.Component {
+
+
+    render(){
+    const listToBuy = ["Cucumber", "Kale", "Tortillas"];
+
+    const listOfItems = listToBuy.map((value,index) => {
+        return(
+
+            <GroceryListItem name={value} key={index}/>
+
+        )
+    })
+        return (
+            <ul >
+                {listOfItems}
+            </ul>
+        )
+    }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <GroceryList />
+
   );
 }
 
